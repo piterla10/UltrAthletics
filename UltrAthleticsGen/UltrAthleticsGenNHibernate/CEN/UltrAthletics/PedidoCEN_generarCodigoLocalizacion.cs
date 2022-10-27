61c87ef65a2cfabfe1fb61c81d1f5891008f7801
Livingrock545 <jmdomenechterol@gmail.com>
2022-10-27 11:26:35 +0200

    Codigo devolucion y localizacion

--- UltrAthleticsGen/UltrAthleticsGenNHibernate/CEN/UltrAthletics/PedidoCEN_generarCodigoLocalizacion.cs
+++ UltrAthleticsGen/UltrAthleticsGenNHibernate/CEN/UltrAthletics/PedidoCEN_generarCodigoLocalizacion.cs
@@ -11,7 +11,7 @@ using UltrAthleticsGenNHibernate.EN.UltrAthletics;
 using UltrAthleticsGenNHibernate.CAD.UltrAthletics;
 
 
-/*PROTECTED REGION ID(usingUltrAthleticsGenNHibernate.CEN.UltrAthletics_Pedido_getCodigoLocalizacion) ENABLED START*/
+/*PROTECTED REGION ID(usingUltrAthleticsGenNHibernate.CEN.UltrAthletics_Pedido_generarCodigoLocalizacion) ENABLED START*/
 //  references to other libraries
 /*PROTECTED REGION END*/
 
@@ -19,12 +19,11 @@ namespace UltrAthleticsGenNHibernate.CEN.UltrAthletics
 {
 public partial class PedidoCEN
 {
-public string GetCodigoLocalizacion (int p_oid)
+public void GenerarCodigoLocalizacion (int p_oid)
 {
-        /*PROTECTED REGION ID(UltrAthleticsGenNHibernate.CEN.UltrAthletics_Pedido_getCodigoLocalizacion) ENABLED START*/
+        /*PROTECTED REGION ID(UltrAthleticsGenNHibernate.CEN.UltrAthletics_Pedido_generarCodigoLocalizacion) ENABLED START*/
 
         // Write here your custom code...
-
         PedidoCEN ped1 = new PedidoCEN ();
         PedidoEN pedEN = ped1.DamePedidoOID (p_oid);
 
@@ -32,10 +31,18 @@ public string GetCodigoLocalizacion (int p_oid)
                 throw new Exception ();
         }
 
-        //AQUI UTILIZARIAMOS LA API DE ALGUNA EMPRESA DE MENSAJERIA
-        //SE SUSTITUYE POR UN CODIGO GENERADO
+        string charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        char[] caracteres = new char [12];
+        Random random = new Random ();
 
-        return pedEN.Seguimiento;
+        for (int i = 0; i < caracteres.Length; i++) {
+                caracteres [i] = charset [random.Next (charset.Length)];
+        }
+
+        string generada = new string(caracteres);
+
+        pedEN.Seguimiento = generada;
+        _IPedidoCAD.ModificarPedido (pedEN);
 
         /*PROTECTED REGION END*/
 }
