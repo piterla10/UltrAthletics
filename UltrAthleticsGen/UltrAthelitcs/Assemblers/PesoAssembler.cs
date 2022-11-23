9d0791280a81541890817f3beebc8f93e1569b60
jzg6 <jzg6@gcloud.ua.es>
2022-11-24 00:08:28 +0100

    Pesos

---
+++ UltrAthleticsGen/UltrAthelitcs/Assemblers/PesoAssembler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UltrAthelitcs.Models;
+using UltrAthleticsGenNHibernate.EN.UltrAthletics;
+
+namespace UltrAthelitcs.Assemblers
+{
+    public class PesoAssembler
+    {
+        public PesoViewModel ConvertENToModelUI(PesoEN en)
+        {
+            PesoViewModel pes = new PesoViewModel();
+            pes.Cantidad = en.Cantidad;
+            
+            return pes;
+
+        }
+
+        public IList<PesoViewModel> ConvertListENToModel(IList<PesoEN> ens)
+        {
+            IList<PesoViewModel> pes = new List<PesoViewModel>();
+            foreach (PesoEN en in ens)
+            {
+                pes.Add(ConvertENToModelUI(en));
+            }
+            return pes;
+
+        }
+    }
+}
