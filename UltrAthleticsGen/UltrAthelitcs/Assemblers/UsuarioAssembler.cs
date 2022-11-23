4695af7bb199c39e4636a0aea2d07c832ec5a99d
jzg6 <jzg6@gcloud.ua.es>
2022-11-23 23:46:00 +0100

    MILIBRO

---
+++ UltrAthleticsGen/UltrAthelitcs/Assemblers/UsuarioAssembler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UltrAthelitcs.Models;
+using UltrAthleticsGenNHibernate.EN.UltrAthletics;
+
+namespace UltrAthelitcs.Assemblers
+{
+    public class UsuarioAssembler
+    {
+        public UsuarioViewModel ConvertENToModelUI(UsuarioEN en)
+        {
+            UsuarioViewModel usu = new UsuarioViewModel();
+            usu.Email = en.Email;
+            usu.Nombre = en.Nombre;
+            return usu;
+
+
+        }
+        public IList<UsuarioViewModel> ConvertListENToModel(IList<UsuarioEN> ens)
+        {
+            IList<UsuarioViewModel> usus = new List<UsuarioViewModel>();
+            foreach (UsuarioEN en in ens)
+            {
+                usus.Add(ConvertENToModelUI(en));
+            }
+            return usus;
+        }
+
+    }
+}
---
+++ UltrAthleticsGen/UltrAthelitcs/Models/UsuarioViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UltrAthelitcs.Models
+{
+    public class UsuarioViewModel
+    {
+        [Display(Prompt = "Nombre del usuario", Description = "Nombre del usuario", Name = "Nombre: ")]
+        public String Email { get; set; }
+
+
+        [Display(Prompt = "Nombre del usuario", Description = "Nombre del usuario", Name = "Nombre: ")]
+        [Required(ErrorMessage = "Debe indicar un nombre para el artículo")]
+        [DataType(DataType.Text, ErrorMessage = "El Nombre debe ser un texto")]
+        [StringLength(maximumLength: 30, ErrorMessage = "El nombre debe tener como maximo 30 caracteres", MinimumLength = 2)]
+        public String Nombre { get; set; }
+
+    }
+}
